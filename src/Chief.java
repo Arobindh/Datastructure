b6e4e5df09878e980b7167685b3ed32cbade8b95
14388 <arobindh@gmail.com>
2019-09-20 11:13:03 -0400

    Adding codes

--- src/Chief.java
+++ src/Chief.java
@@ -3,6 +3,12 @@ public class Chief
 {
 	public static void main(String args[])
 	{
-		
+		Doubly_linked_list dll = new Doubly_linked_list();
+        dll.append(6); 
+        dll.push(7); 
+        dll.push(1); 
+        dll.append(4); 
+        dll.PushAfter(dll.head.next, 8); 
+        dll.printlist(dll.head); 
 	}
 }
